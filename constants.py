3c9f35873d2c4301df4123fed45251d0e2972242
Alex <thespartiate@gmail.com>
2010-03-17 19:22:02 -0400

    changed from comparing square roots to comparing squares.

--- constants.py
+++ constants.py
@@ -6,11 +6,13 @@ Created on Mar 16, 2010
 #In pixels, the radius that bots may capture in.
 CAPTURE_RADIUS = 50
 
+CAPTURE_RADIUS_SQUARE = CAPTURE_RADIUS**2
+
 #The size of the arena.
 SIZE = WIDTH, HEIGHT = 1000,1000
 
 #Number of bots.
-NUM_BOTS = 30
+NUM_BOTS = 100
 
 #Frames per second.
 FPS = 60
